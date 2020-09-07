a4af10313c5c33bfe39ed46bc4643e0f15c38c02
Sven Kilberg <sven.kilberg@gmail.com>
2020-09-07 11:41:51 +0200

    Some pseudo code added

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict'
+
+function authenticate() {
+
+    // Om localStorage user and password = OK kalla på createLogedinview.
+
+    // Om localStorage user eller password != OK kalla på creatErrorView.
+
+    // Om localStorage är tomt kalla på createMainView.
+    
+}
+
+function createLoggedinView()  {
+
+    // Ändra mainDiv innerHTML.
+    // <H4> med texten "Välkommen [skriv ut usernamnet från variabel] !"
+    // <p> med texten "Det här är din personliga sida".
+    // Lägg till en knapp längst ner med texten "Logga ut".
+}
+
+function createErrorView() {
+
+    // Ändra mainDiv innerHTML.
+    // <H4> med texten "Fel inloggning !"
+    // Beroende på vad som är fel skriv ut en eller båda av:
+    // <p> med texten "Användaren [skriv ut usernamnet från variabel] är fel."
+    // <p> med texten "Användaren [skriv ut password från variabel] är fel."
+    // Lägg till en knapp längst ner med texten "Tillbaka".
+}
+    
